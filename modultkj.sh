857f583c2a026d616ac67c5c4eff3382568de83b
hirogans <65782788+hirogans@users.noreply.github.com>
2020-06-05 00:57:28 +0700

    Create modultkj.sh

---
+++ modultkj.sh
@@ -0,0 +1,270 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# ✿͜͡ঔৣ✪➵͜͡Hiro-IDঔৣ͜᷍🌹
+# recode gw sumpahin mati
+sleep 2
+echo $cy"
++--------------------------------------------------+
+|         Module Pembelajaran Tkj                  |
++--------------------------------------------------+"
+sleep 2
+echo $i"+------------------------------±"
+echo $cy"(/) Membuka module... "
+sleep 2
+echo $i"+------------------------------±"
+echo $cy"(/) Menyiapkan Aplikasi... "
+sleep 2
+echo $i"+------------------------------±"
+echo $cy"(/) Mohon tunggu... "
+sleep 3
+echo $i"+------------------------------±"
+echo $cy"(/) Penyiapan selesai... "
+sleep 2
+echo $i"+------------------------------±"
+echo ""
+echo $bi
+echo -n "(+) Masukan Nama Kamu : ";read nm
+sleep 2
+echo ""
+echo $bi"(-) Hallo $nm selamat datang dan selamat membaca "
+sleep 2
+echo ""
+echo $bi"(:) Sebelumnya Perkenalkan Nama Saya Teguh Sabila "
+sleep 2
+echo ""
+echo $bi"(!) Kamu Akan Di Arahkan Menuju Beranda Module "
+sleep 2
+echo ""
+echo -n "(?) Klik Enter untuk Melanjutkan [enter] ";read vv
+sleep 1
+clear
+echo
+echo
+echo
+sleep 2
+echo $i"["$bi"•"$i"]"$ku"─────────────────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"               Modul Pembelajaran TKJ         "$i"         |"
+echo $i" |"$ku"───────────────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$i" Teguh Sabila                            "$i"     |"
+echo $i" |"$pu" FACEBOOK :"$ku" Hiro ID"$i"                                    |"
+echo $i" |"$pu" Asal Sekolah  :"$cy"~ SMK KOMPUTER INDONESIA     "$i"          |"
+echo $i" |"$pu" Contack WhatsApp :"$i" 089530639515"$i"                       |"
+echo $i" |"$pu" Contack Gmail :"$i" teguhsabila22@gmail.com"$i"               |"
+echo $i"["$bi"•"$i"]"$ku"─────────────────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"Silahkan Pilih Modul Yang Tersedia"
+echo
+echo $i"─────────────────────────────────────────"
+echo $i"|"$me" 1"$i" |"$ku" PENGERTIAN KOMPUTER DAN JARINGAN  "$i"|"
+echo $i"|"$me" 2"$i" |"$ku" PENGERTIAN IP ADDRESS             "$i"|"
+echo $i"─────────────────────────────────────────"
+echo $i"|"$me" I"$i" |"$ku" INSTALL BAHAN TERMUX              "$i"|"
+echo $i"|"$me" E"$i" |"$ku" KELUAR/EXIT PROJECT               "$i"|"
+echo $i"─────────────────────────────────────────"
+echo
+echo
+echo $cy"┌==="$ku"["$bi"✿͜͡ঔৣ✪➵͜͡Hiro-IDঔৣ͜᷍🌹"$ku"]"$cy"======"$ku"["$bi""Pilih Nomornya""$ku"]"
+echo $cy"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f small  "TUNGGU..."|lolcat
+sleep 2
+clear
+sleep 2
+echo $cy"
++--------------------------------------------+
+      PENGERTIAN KOMPUTER DAN JARINGAN
++--------------------------------------------+"
+sleep 2
+echo $i"
+Pengertian Komputer
+-------------------
+Apa yang dimaksud dengan komputer? Secara
+umum, pengertian Komputer adalah suatu
+perangkat elektronik yang dapat digunakan
+untuk mengolah data sesuai dengan prosedur
+yang telah dirumuskan sebelumnya sehingga
+menghasilkan informasi bermanfaat bagi
+penggunanya.
+
+Definisi komputer adalah alat elektronik yang
+terdiri dari rangkaian berbagai komponen yang
+saling terhubung sehingga membentuk suatu
+sistem kerja. Sistem di dalam komputer
+tersebut dapat melakukan pekerjaan secara
+otomatis berdasarkan program yang
+diperintahkan kepadanya sehingga mampu
+menghasilkan informasi berdasarkan data dan
+program yang ada.
+
+Pengertian Jaringan Komputer
+----------------------------
+Jaringan komputer (jaringan) adalah jaringan
+telekomunikasi yang memungkinkan antar
+komputer untuk saling berkomunikasi dengan
+bertukar data. Tujuan dari jaringan komputer
+adalah agar dapat mencapai tujuannya, setiap
+bagian dari jaringan komputer dapat meminta
+dan memberikan layanan (service). Pihak yang
+meminta/menerima layanan disebut klien
+(client) dan yang memberikan/mengirim
+layanan disebut peladen (server). Desain ini
+disebut dengan sistem client-server, dan
+digunakan pada hampir seluruh
+aplikasijaringan komputer.
+
+Dua buah komputer yang masing-masing
+memiliki sebuah kartu jaringan, kemudian
+dihubungkan melalui kabel maupun
+nirkabelsebagai medium transmisi data, dan
+terdapat perangkat lunak sistem operasi
+jaringan akan membentuk sebuah jaringan
+komputer yang sederhana. Apabila ingin
+membuat jaringan komputer yang lebih luas
+lagi jangkauannya, maka diperlukan peralatan
+tambahan seperti Hub, Bridge, Switch, Router,
+Gateway sebagai peralatan interkoneksinya.
+
++------------------+
+ttd : Teguh Sabila
+© 2020.inc
++------------------+"
+echo ""
+sleep 4
+echo $me""
+read -p "Keluar Aplikasi[y/n] " su
+if [ $su = y ]
+then
+sleep 1
+echo ""
+echo $me"(/) Terima kasih sampai jumpa lagi "
+sleep 3
+exit
+fi
+if [ $su = n ]
+then
+sh modultkj.sh
+fi
+
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f small "TUNGGU..."|lolcat
+sleep 2
+clear
+echo $cy"
++--------------------------------------------+
+          PENGERTIAN IP ADDRESS
++--------------------------------------------+"
+sleep 2
+echo $i"
+IP Address
+----------
+IP Address adalah alamat yang diberikan ke
+jaringan dan peralatan jaringan yang
+menggunakan protokol TCP/IP. IP Address
+terdiri atas 32 bit (biary digit atau bilangan
+duaan) angka biner yang dibagi dalam 4 oket
+(byte) terdiri dari 8 bit. Setiap bit
+mempresentasikan bilangan desimal mulai
+dari 0 sampai 255.
+
+Jenis-jenis IP Address terdiri dari :
+
+1. IP Public
+
+Public bit tertinggi range address bit network address
+kelas A 0 0 – 127* 8
+kelas B 10 128 – 191 16
+kelas C 110 192 – 223 24
+kelas D 1110 224 – 239 28
+
+2. Privat
+
+IP Privat ini dapat digunakan dengan bebas
+tetapi tidak dikenal pada jaringan internet
+global. Karena itu biasa dipergunakan pada
+jaringan tertutup yang tidak terhubung ke
+internet, misalnya jaringan komputer ATM.
+
+10.0.0.0 – 10.255.255.255
+172.16.0.0 – 172.31.255.255
+192.168.0.0 – 192.168.255.255
+
+Kesimpulan
+1.0.0.0 - 126.0.0.0 : Kelas A.
+127.0.0.0 : Loopback network.
+128.0.0.0 - 191.255.0.0 : Kelas B.
+192.0.0.0 - 223.255.255.0 : Kelas C.
+224.0.0.0 = 240.0.0.0 : Class E, reserved.
+3. Ipv6
+terdiri dari 16 oktet, contoh :
+A524:72D3:2C80:DD02:0029:EC7A:002B:EA73
+
++-----------------+
+ttd : Teguh Sabila
+© 2020.inc
++-----------------+"
+echo ""
+sleep 4
+echo $me""
+read -p "Keluar Aplikasi[y/n] " su
+if [ $su = y ]
+then
+sleep 1
+echo ""
+echo $me"(/) Terima kasih sampai jumpa lagi "
+sleep 3
+exit
+fi
+
+if [ $pil = I ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f small " S E L E S A I . . "|lolcat
+sh modultkj.sh
+fi
+
+if [ $pil = E ]
+then
+clear
+figlet -f small "Keluar..."|lolcat
+sleep 2
+clear
+echo $cy"Terima Kasih Telah Menggunakan Project saya"
+sleep 2
+echo $i"Jika Ada Yang Kurang Paham Bisa Tanyakan Saya"
+sleep 2
+echo $ku"WhatsApp :"$i" 089530639515"
+echo $ku"Facebook :"$i" Hiro ID"
+sleep 2
+echo $pur">> Berikan saran dan komentar anda <<"
+sleep 2
+exit
+fi
